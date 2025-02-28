130d58ce06c3a9d79d8a4dfa23f39c6f13130b92
BPP4634 <beagutara@alum.us.es>
2025-02-28 17:44:10 +0100

    feat: :sparkles: Entidad de servicio creada
    Se ha creado la entidad de servicio con sus correspondientes restricciones y relaciones.
    
    Refs: #45

---
+++ src/main/java/acme/entities/services/Service.java
@@ -0,0 +1,64 @@
+
+package acme.entities.services;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.OneToMany;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.datatypes.Money;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoney;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import acme.client.components.validation.ValidUrl;
+import acme.entities.reviews.Review;
+
+public class Service extends AbstractEntity {
+
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				name;
+
+	@Mandatory
+	@ValidUrl
+	@Automapped
+	private String				picture;
+
+	@Mandatory
+	@ValidNumber(min = 0., integer = 2, fraction = 1)
+	@Automapped
+	private Double				avgDwellTime;
+
+	@Optional
+	@ValidString(min = 6, max = 6, pattern = "^[A-Z]{4}-[0-9]{2}$")
+	@Automapped
+	@Column(unique = true)
+	private String				promotionCode;
+
+	@Optional
+	@ValidMoney
+	@Automapped
+	private Money				money;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+	@Optional
+	@Valid
+	@OneToMany
+	private List<Review>		reviews;
+
+}
