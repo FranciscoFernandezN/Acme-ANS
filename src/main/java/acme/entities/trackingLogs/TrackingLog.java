d6efe698d8036ffec0815eb80569c0502c34c428
ManuelJBM <manbenmer1@alum.us.es>
2025-03-08 19:14:40 +0100

    feat: Añadida la entidad TrackingLog

---
+++ src/main/java/acme/entities/trackingLogs/TrackingLog.java
@@ -0,0 +1,67 @@
+
+package acme.entities.trackingLogs;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Transient;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.datatypes.Moment;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidScore;
+import acme.client.components.validation.ValidString;
+import acme.entities.claims.Claim;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class TrackingLog extends AbstractEntity {
+
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Automapped
+	private Moment				lastUpdateMoment;
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				step;
+
+	@Mandatory
+	@ValidScore
+	@Automapped
+	private Double				resolutionPercentage;
+
+	@Mandatory
+	@ValidString
+	@Automapped
+	private String				resolution;
+
+	// Derived attributes -----------------------------------------------------
+
+
+	@Transient
+	private Boolean getAcceptance() {
+		return this.claim.getAccepted();
+	}
+
+	// Relationships ----------------------------------------------------------
+
+
+	@Mandatory
+	@Valid
+	@OneToOne
+	private Claim 				claim;
+
+}
