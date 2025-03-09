12452f2cc21c40e69b287404ad5d39e9c4aba282
BPP4634 <beagutara@alum.us.es>
2025-03-09 16:05:38 +0100

    feat: :sparkles: Entidad de customer añadida
    Se ha añadido la entidad de customer, la cuál representa a los compradores de billetes de los vuelos.
    
    Refs: #80

---
+++ src/main/java/acme/realms/Customer.java
@@ -0,0 +1,58 @@
+
+package acme.realms;
+
+import javax.persistence.Column;
+
+import org.checkerframework.common.aliasing.qual.Unique;
+
+import acme.client.components.basis.AbstractRole;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+
+public class Customer extends AbstractRole {
+
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Unique
+	@Mandatory
+	@ValidString(min = 8, max = 9, pattern = "^[A-Z]{2-3}\\d{6}$")
+	@Column(unique = true)
+	private String				identifier;
+
+	@Mandatory
+	@ValidString(min = 6, max = 15, pattern = "^\\+?\\d{6,15}$")
+	@Automapped
+	private String				phoneNumber;
+
+	@Mandatory
+	@ValidString
+	@Automapped
+	private String				physicalAddress;
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				city;
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				country;
+
+	@Optional
+	@ValidNumber(min = 0., max = 500000., integer = 6, fraction = 0)
+	@Automapped
+	private Integer				earnedPoints;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+}
