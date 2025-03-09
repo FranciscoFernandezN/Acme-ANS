115baf5f0fb18825877a7c151fddd59e1ff4b49f
FranciscoFernandezN <frafernog@alum.us.es>
2025-03-09 21:40:21 +0100

    feat: Añadido datos para managers

--- src/main/java/acme/realms/Manager.java
+++ src/main/java/acme/realms/Manager.java
@@ -6,6 +6,8 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.Valid;
 
 import org.checkerframework.common.aliasing.qual.Unique;
@@ -47,7 +49,7 @@ public class Manager extends AbstractRole {
 
 	@Mandatory
 	@ValidMoment(past = true)
-	@Automapped
+	@Temporal(TemporalType.DATE)
 	private Date				birth;
 
 	@Optional
