cc16da3d7ff3632623d12abbd295ea2d8da1d20d
BPP4634 <beagutara@alum.us.es>
2025-03-09 17:10:32 +0100

    feat: :sparkles: Entidad de passenger añadida
    Se ha añadido la entidad de passenger, la cuál representa a los pasajeros de los vuelos.
    
    Refs: #82

---
+++ src/main/java/acme/realms/Passenger.java
@@ -0,0 +1,59 @@
+
+package acme.realms;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+import acme.client.components.basis.AbstractRole;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidEmail;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Passenger extends AbstractRole {
+
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Mandatory
+	@ValidString
+	@Automapped
+	private String				fullName;
+
+	@Mandatory
+	@ValidEmail
+	@Automapped
+	private String				email;
+
+	@Mandatory
+	@ValidString(min = 6, max = 9, pattern = "^[A-Z0-9]{6,9}$")
+	@Column(unique = true)
+	private String				passportNumber;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Automapped
+	private Date				dateOfBirth;
+
+	@Optional
+	@ValidString(max = 50)
+	@Automapped
+	private String				specialNeeds;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+}
