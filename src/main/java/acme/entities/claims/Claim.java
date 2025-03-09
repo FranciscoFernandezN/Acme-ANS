a777f35233662e6079fab3711d87095232cbfafe
ManuelJBM <manbenmer1@alum.us.es>
2025-03-09 21:18:22 +0100

    fix: Cambiado el tipo y anotación de registrationMoment y eliminado el valid del Boolean

--- src/main/java/acme/entities/claims/Claim.java
+++ src/main/java/acme/entities/claims/Claim.java
@@ -1,14 +1,17 @@
 
 package acme.entities.claims;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
-import acme.client.components.datatypes.Moment;
 import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.ValidEmail;
@@ -31,8 +34,8 @@ public class Claim extends AbstractEntity {
 
 	@Mandatory
 	@ValidMoment(past = true)
-	@Automapped
-	private Moment				registrationMoment;
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				registrationMoment;
 
 	@Mandatory
 	@ValidEmail
@@ -51,7 +54,6 @@ public class Claim extends AbstractEntity {
 	private ClaimType			claimType;
 
 	@Mandatory
-	@Valid
 	@Automapped
 	private Boolean				accepted;
 
