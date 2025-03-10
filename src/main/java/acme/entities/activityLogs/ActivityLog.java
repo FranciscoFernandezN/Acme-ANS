711db6bd76c981b851528a22894840432aea228e
JUAN VARO <juavarver@alum.us.es>
2025-03-10 18:59:46 +0100

    feat: Añadida la entidad ActivityLog

---
+++ src/main/java/acme/entities/activityLogs/ActivityLog.java
@@ -0,0 +1,62 @@
+
+package acme.entities.activityLogs;
+
+import java.util.Date;
+
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidString;
+import acme.entities.legs.Leg;
+import acme.realms.FlightCrewMember;
+
+public class ActivityLog extends AbstractEntity {
+
+	// Serialisation version --------------------------------------------------
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				registrationMoment;
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				typeOfIncident;
+
+	@Mandatory
+	@ValidString(max = 255)
+	@Automapped
+	private String				description;
+
+	@Mandatory
+	@Min(0)
+	@Max(10)
+	@Automapped
+	private Integer				severityLevel;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+	@Mandatory
+	@Valid
+	@ManyToOne
+	private FlightCrewMember	flightCrewMember;
+
+	@Mandatory
+	@Valid
+	@ManyToOne
+	private Leg					leg;
+
+}
