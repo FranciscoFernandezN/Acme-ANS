c7f88831ad1612c10f58ef817cfd1778c6b8e1a6
BPP4634 <beagutara@alum.us.es>
2025-03-10 18:25:15 +0100

    feat: :sparkles: Entidad booking creada
    Se ha creado la entidad booking siguiendo los requisitos dados.
    
    Refs: #81

---
+++ src/main/java/acme/entities/bookings/Booking.java
@@ -0,0 +1,88 @@
+
+package acme.entities.bookings;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+
+import org.checkerframework.common.aliasing.qual.Unique;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.datatypes.Money;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidMoney;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import acme.entities.flights.Flight;
+import acme.realms.Customer;
+import acme.realms.Passenger;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Booking extends AbstractEntity {
+
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Unique
+	@Mandatory
+	@ValidString(min = 6, max = 8, pattern = "^[A-Z0-9]{6,8}$")
+	@Column(unique = true)
+	private String				locatorCode;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				purchaseMoment;
+
+	@Mandatory
+	@Enumerated(EnumType.STRING)
+	@Automapped
+	private TravelClass			travelClass;
+
+	@Mandatory
+	@ValidMoney(min = 0)
+	@Automapped
+	private Money				price;
+
+	@Optional
+	@ValidNumber(integer = 4, fraction = 0)
+	@Automapped
+	private Integer				lastNibble;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Flight				flight;
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Passenger			passenger;
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Customer			customer;
+
+}
