ac4d37698fa7afdec96b1eb04f6d0dee23967c2b
estegn10 <estegn10@gmail.com>
2025-03-10 21:36:55 +0100

    feat: :sparkles: Entidad Tarea añadida
    Los registros de mantenimiento se basan en tareas. Una tarea es un deber operativo específico predefinido que lleva a cabo un técnico en las aeronaves.

---
+++ src/main/java/acme/entities/tasks/Task.java
@@ -0,0 +1,70 @@
+
+package acme.entities.tasks;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.ValidString;
+import acme.entities.maintenancerecords.MaintenanceRecord;
+import acme.realms.Technician;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Task extends AbstractEntity {
+
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Mandatory
+	@Valid
+	@Enumerated(EnumType.STRING)
+	@Automapped
+	private TaskType			type;
+
+	@Mandatory
+	@ValidString(max = 255)
+	@Automapped
+	private String				description;
+
+	@Mandatory
+	@Min(0)
+	@Max(10)
+	@Automapped
+	private int					priority;
+
+	@Mandatory
+	@Min(0)
+	@Automapped
+	private double				estimatedDuration;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+	@Mandatory
+	@Valid
+	@ManyToOne
+	@Automapped
+	private Technician			assignedTechnician;
+
+	@Mandatory
+	@Valid
+	@ManyToOne
+	@Automapped
+	private MaintenanceRecord	relatedMaintenance;
+
+}
