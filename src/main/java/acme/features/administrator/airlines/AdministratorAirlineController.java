0bc026148859c6575a718fff5eff53a130a47001
estegn10 <estegn10@gmail.com>
2025-03-24 19:40:15 +0100

    feat: :sparkles: Operaciones de administrador en airlines clases
    Se ha añadido las clases necesarias para las operaciones que un administrador puede realizar en Airlines. Bien sea crear, modificar, listar o enseñar una aerolínea.

---
+++ src/main/java/acme/features/administrator/airlines/AdministratorAirlineController.java
@@ -0,0 +1,38 @@
+
+package acme.features.administrator.airlines;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.principals.Administrator;
+import acme.client.controllers.AbstractGuiController;
+import acme.entities.airlines.Airline;
+
+public class AdministratorAirlineController extends AbstractGuiController<Administrator, Airline> {
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private AdministratorAirlineListService		listService;
+
+	@Autowired
+	private AdministratorAirlineShowService		showService;
+
+	@Autowired
+	private AdministratorAirlineUpdateService	updateService;
+
+	@Autowired
+	private AdministratorAirlineCreateService	createService;
+
+	// Constructors -----------------------------------------------------------
+
+
+	@PostConstruct
+	protected void initialise() {
+		super.addBasicCommand("list", this.listService);
+		super.addBasicCommand("show", this.showService);
+		super.addBasicCommand("update", this.updateService);
+		super.addBasicCommand("create", this.createService);
+	}
+
+}
---
+++ src/main/java/acme/features/administrator/airlines/AdministratorAirlineCreateService.java
@@ -0,0 +1,72 @@
+
+package acme.features.administrator.airlines;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.models.Dataset;
+import acme.client.components.principals.Administrator;
+import acme.client.helpers.MomentHelper;
+import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
+import acme.entities.airlines.Airline;
+
+@GuiService
+public class AdministratorAirlineCreateService extends AbstractGuiService<Administrator, Airline> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private AdministratorAirlineRepository repository;
+
+	// AbstractGuiService interface -------------------------------------------
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Airline airline;
+
+		airline = new Airline();
+
+		super.getBuffer().addData(airline);
+	}
+
+	@Override
+	public void bind(final Airline airline) {
+		Date now;
+
+		now = MomentHelper.getCurrentMoment();
+		super.bindObject(airline, "name", "IATACode", "website", "type", "email", "contactNumber");
+		airline.setFoundationMoment(now);
+	}
+
+	@Override
+	public void validate(final Airline airline) {
+		boolean confirmation;
+
+		confirmation = super.getRequest().getData("confirmation", boolean.class);
+		super.state(confirmation, "confirmation", "acme.validation.confirmation.message");
+	}
+
+	@Override
+	public void perform(final Airline airline) {
+		this.repository.save(airline);
+	}
+
+	@Override
+	public void unbind(final Airline airline) {
+		Dataset dataset;
+
+		dataset = super.unbindObject(airline, "name", "IATACode", "website", "type", "email", "contactNumber");
+
+		super.getResponse().addData(dataset);
+
+	}
+
+}
