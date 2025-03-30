188e45524f4c6878319b268ca1e5257610d4b706
FranciscoFernandezN <frafernog@alum.us.es>
2025-03-30 14:14:02 +0200

    feat: Listar configuración del sistema

---
+++ src/main/java/acme/features/administrator/supportedCurrency/AdministratorSupportedCurrencyCreateService.java
@@ -0,0 +1,77 @@
+package acme.features.administrator.supportedCurrency;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.models.Dataset;
+import acme.client.components.principals.Administrator;
+import acme.client.helpers.PropertyHelper;
+import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
+import acme.entities.flights.Flight;
+import acme.entities.legs.Leg;
+import acme.entities.supportedCurrency.SupportedCurrency;
+
+@GuiService
+public class AdministratorSupportedCurrencyCreateService extends AbstractGuiService<Administrator, SupportedCurrency> {
+	
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private AdministratorSupportedCurrencyRepository scr;
+
+	// AbstractGuiService interface -------------------------------------------
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(super.getRequest().getPrincipal().hasRealmOfType(Administrator.class));
+	}
+
+	@Override
+	public void load() {
+		SupportedCurrency supportedCurrency;
+		
+		supportedCurrency = new SupportedCurrency();
+
+		super.getBuffer().addData(supportedCurrency);
+	}
+	
+	@Override
+	public void bind(final SupportedCurrency supportedCurrency) {
+		super.bindObject(supportedCurrency, "currencyName");
+	}
+
+	@Override
+	public void validate(final SupportedCurrency supportedCurrency) {
+		String defaultCurrency = PropertyHelper.getRequiredProperty("acme.currency.default", String.class);
+		
+		List<SupportedCurrency> supportedCurrencies = this.scr.findAllSupportedCurrencies();
+		List<String> currencyNames = supportedCurrencies.stream().map(sp -> sp.getCurrencyName()).toList();
+		
+		if(supportedCurrency.getCurrencyName() != null) {
+			super.state(!supportedCurrency.getCurrencyName().equals(defaultCurrency), "currencyName", "administrator.supported-currency.create.is-default-currency");
+			super.state(!currencyNames.contains(supportedCurrency.getCurrencyName()), "currencyName", "administrator.supported-currency.create.already-exists-currency");
+		}
+		
+	}
+
+	@Override
+	public void perform(final SupportedCurrency supportedCurrency) {
+		this.scr.save(supportedCurrency);
+	}
+
+	@Override
+	public void unbind(final SupportedCurrency supportedCurrency) {
+		Dataset dataset;
+		
+		String defaultCurrency = PropertyHelper.getRequiredProperty("acme.currency.default", String.class);
+		
+		dataset = super.unbindObject(supportedCurrency, "currencyName");
+		dataset.put("isDefaultCurrency", supportedCurrency.getCurrencyName() == null ? "N/A" : supportedCurrency.getCurrencyName().equals(defaultCurrency));
+
+		super.getResponse().addData(dataset);
+	}
+	
+}
--- src/main/resources/WEB-INF/views/fragments/menu.jsp
+++ src/main/resources/WEB-INF/views/fragments/menu.jsp
@@ -44,6 +44,8 @@
 			<acme:menu-suboption code="master.menu.administrator.populate-db-sample" action="/administrator/system/populate-sample"/>			
 			<acme:menu-separator/>
 			<acme:menu-suboption code="master.menu.administrator.shut-system-down" action="/administrator/system/shut-down"/>
+			<acme:menu-separator/>
+			<acme:menu-suboption code="master.menu.administrator.list-supported-currencies" action="/administrator/supported-currency/list"/>
 		</acme:menu-option>
 
 		<acme:menu-option code="master.menu.provider" access="hasRealm('Provider')">
