e3cc452a8df25fd0efbd4730a5954b45aab6988a
JUAN VARO <juavarver@alum.us.es>
2025-05-21 20:04:41 +0200

    feat: Añadir índices de flightCrewMember, flightAssignment y activityLog

--- src/main/java/acme/entities/flightassignments/FlightAssignment.java
+++ src/main/java/acme/entities/flightassignments/FlightAssignment.java
@@ -6,7 +6,9 @@ import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
@@ -25,6 +27,9 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@Table(indexes = {
+	@Index(columnList = "flight_crew_member_id"), @Index(columnList = "leg_id"), @Index(columnList = "isDraftMode"), @Index(columnList = "flight_crew_member_id,isDraftMode"), @Index(columnList = "leg_id,isDraftMode")
+})
 public class FlightAssignment extends AbstractEntity {
 
 	// Serialisation version --------------------------------------------------
@@ -56,7 +61,6 @@ public class FlightAssignment extends AbstractEntity {
 	@Mandatory
 	@Automapped
 	private Boolean				isDraftMode			= true;
-
 
 	// Derived attributes -----------------------------------------------------
 	// Relationships ----------------------------------------------------------
